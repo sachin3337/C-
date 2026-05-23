6add7795145755a627b98ab83cdd3cdb15c9f2c1
sachin3733 <sachinsubhashghusinge@gmail.com>
2026-05-24 01:03:45 +0530

    Changing the name

--- class/Day08/o04DeclerationString.c
+++ class/Day08/o04DeclerationString.c
@@ -1,27 +1,31 @@
 #include<stdio.h>
-//different declarations of string
+
+// Demonstration of different string declarations in C
 int main()
 {
+    // String as a char array with explicit '\0'
+    // Total size = 6 bytes (5 chars + '\0')
+    char str[6] = {'A','B','C','D','E','\0'};  
+    printf("\nString str => ");
+    printf("%s", str);                     // Output: ABCDE
+    printf("\nSize of string str = %d", sizeof(str)); // 6
 
-    //string is char array having last element as '\0' =>end of string 
-    char str[6] ={'A','B', 'C', 'D' , 'E' , '\0'};// stack section// string
-    printf("\n  String str=> "); 
-    printf("%s", str );//ABCDE
-    printf("\nsize of string str=%d", sizeof(str)); //6 * 1=> 6 
-    char str1[] = {'A','B', 'C', 'D' , 'E' , '\0'}; // string // stack section
-    //             0    1    2   3  4
-    char str2[5]= {'A','B', 'C'        };// stack section //string //partial initialization => when we give partial initilization
-    //for char array it creates the string
-    printf("\n%s", str1 );//ABCDE
-    printf("\nsize of string str1=%d", sizeof(str1)); //6 * 1=> 6 
-    //Note:- when we pass the string to the sizeof op it generates the result includeing 
-    //'\0'=> end of string.
+    // String declared without explicit size (compiler counts elements)
+    char str1[] = {'A','B','C','D','E','\0'};  
+    printf("\nString str1 => %s", str1);  // Output: ABCDE
+    printf("\nSize of string str1 = %d", sizeof(str1)); // 6
 
-    printf("\n str2=%s", str2); //ABC
-    printf("\nsize of string str2=%d", sizeof(str2)); //6 * 1=> 6 
-    char str3[6]= "ABCD";// stack section //string 
-    printf("\n str3=%s", str3); //ABC
-    printf("\nsize of string str3=%d", sizeof(str3)); //6 * 1=> 6 
-  
- 
+    // Partial initialization: remaining characters auto-filled with '\0'
+    // str2 = {'A','B','C','\0','\0'}
+    char str2[5] = {'A','B','C'};
+    printf("\nString str2 => %s", str2);  // Output: ABC
+    printf("\nSize of string str2 = %d", sizeof(str2)); // 5
+
+    // String initialized using string literal
+    // Compiler inserts '\0' automatically
+    char str3[6] = "ABCD"; // "ABCD\0"
+    printf("\nString str3 => %s", str3);  // Output: ABCD
+    printf("\nSize of string str3 = %d", sizeof(str3)); // 6
+
+    return 0;
 }
